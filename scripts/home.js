12964a73d4df0ce0d873f94450d9d87701a9688a
Gabriel Junges Baratto <gabrieljbaratto@gmail.com>
2021-04-08 17:34:42 -0300

    Valor das entradas definidas como > 0. Inputs guardados localmente

--- scripts/home.js
+++ scripts/home.js
@@ -1,13 +1,30 @@
 const inputs = document.getElementsByTagName("input");
-if(localStorage.getItem("linear")){
-    inputs[0].value = Number(localStorage.getItem("timer"))
+if(localStorage.getItem("timer")){
+    let cache = localStorage.getItem("timer").split(',').map(Number);
+    inputs[0].value= cache[0];
+    inputs[1].value= cache[1];
+    inputs[2].value= cache[2]
 }
 document.addEventListener("click",(event)=>{
     if(event.target.id == "up_trabalho"){
         inputs[0].value = Number(inputs[0].value) + 1;
     }
-    else if(event.target.id == "down_trabalho"){
+    else if(event.target.id == "down_trabalho" && inputs[0].value>0){
         inputs[0].value = Number(inputs[0].value) - 1;
     }
-    localStorage.setItem("timer",inputs[0].value);
+
+    else if(event.target.id == "up_pausa"){
+        inputs[1].value = Number(inputs[1].value) + 1;
+    }
+    else if(event.target.id == "down_pausa" && inputs[1].value>0){
+        inputs[1].value = Number(inputs[1].value) - 1;
+    }
+
+    else if(event.target.id == "up_secoes"){
+        inputs[2].value = Number(inputs[2].value) + 1;
+    }
+    else if(event.target.id == "down_secoes" && inputs[2].value>0){
+        inputs[2].value = Number(inputs[2].value) - 1;
+    }
+    localStorage.setItem("timer",[inputs[0].value, inputs[1].value, inputs[2].value]);
 });
